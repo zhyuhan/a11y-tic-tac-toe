db22450210d1b968637f3d6977c36ed8683f55f8
Yu Han Zhang <yuhan@zhyuhan.me>
2023-02-10 13:29:49 +0800

    chore: add a11y testing plugins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  const ReactDOM = require('react-dom')
+  const axe = require('@axe-core/react')
+  axe(React, ReactDOM, 1000)
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
