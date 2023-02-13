8d3fab0982e5c9dc5c0d8b7571e9532ee9fc4055
Yu Han Zhang <yuhan@zhyuhan.me>
2023-02-13 19:53:50 +0800

    feat: fetch game history on home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { Fetcher, SWRConfig } from 'swr'
 
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
   const ReactDOM = require('react-dom')
@@ -8,6 +10,18 @@ if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
   axe(React, ReactDOM, 1000)
 }
 
+const fetcher: Fetcher = (url: string) => fetch(url).then((r) => r.json())
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: fetcher,
+      }}
+    >
+      <Navbar />
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
